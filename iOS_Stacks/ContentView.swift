8c98d488903324f1f47e8bbededd1ed7aaf6f847
Iclementem <iclementem@outlook.es>
2022-08-01 10:02:25 +0200

    Initial Commit

---
+++ iOS_Stacks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS_Stacks
+//
+//  Created by Iván Clemente Moreno on 1/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS_Stacks/iOS_StacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_StacksApp.swift
+//  iOS_Stacks
+//
+//  Created by Iván Clemente Moreno on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_StacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
